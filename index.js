3c537e42e020517fe79fa0e39754ab846a07fe77
Jonsesuper123 <jonas.glenndal@gmail.com>
2024-01-24 10:12:20 +0100

    added Canny Cat

---
+++ index.js
@@ -0,0 +1,17 @@
+
+/*console.log("HELLO");
+setTimeout(function(){
+    console.log("THIS IS");
+}, 2000);
+console.log("DOG");*/
+
+/*styleTimer() {
+    const wrapperEl = document.querySelector(".wrapper");
+
+    setTimeout(function() {
+        wrapperEl.style.color = "hsla(0, 50%, 50%, 0.9)";
+        wrapperEl.style.backgroundColor = "var(--lightColor)";
+    }, 2000);
+}
+
+styleTimer()*/
